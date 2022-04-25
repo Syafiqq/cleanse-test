720500b2d0a2a85180c919ab4774dc25934f92b6
msyafiqq <muhammad.syafiq@geniebook.com>
2022-04-25 12:08:25 +0700

    initial commit

---
+++ cleanse-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cleanse-test
+//
+//  Created by engineering on 25/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
