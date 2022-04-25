821faa61091b633d0a984b74cc710fcad588d42b
msyafiqq <muhammad.syafiq@geniebook.com>
2022-04-25 13:16:44 +0700

    Step 7: Add dependency

--- cleanse-test/ViewController.swift
+++ cleanse-test/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Step 14: In order to get this object from dependency we can use two option.
+    // 1. Register the ViewController to the dependency graph
+    // 2. Using the property injection
+    // We can't use the number 1 because the VC intitialization is not created by us, so for this sceneario we use the property injector
+    var repoA: RepositoryA?
+    var repoB: RepositoryB?
+    var repoC: RepositoryC?
+    var repoD: RepositoryD?
 
     override func viewDidLoad() {
         super.viewDidLoad()
